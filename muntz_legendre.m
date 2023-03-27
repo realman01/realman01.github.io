edf5ad2be13558b922eee76a01928732c007b107
realman01 <114251281+realman01@users.noreply.github.com>
2023-03-27 20:11:20 +0800

    Update muntz_legendre.m

--- muntz_legendre.m
+++ muntz_legendre.m
@@ -1,4 +1,5 @@
 function [val, diff] = muntz_legendre(lambda,x)
+% muntz_legendre(lambda,x) evaluate Müntz Legendre polynomials and their first-order derivatives for all lambda.
 % Input:
 %   x: N-vector, the nodes in which need to be estimated
 %   lambda: n-vector, the muntz poly orders
@@ -14,24 +15,11 @@ for k = 1 : length(lambda)
    val(:,k) = legendre(k,lambda,x);
 end
 
-% if nargout > 1
-%    disp('Caculating derivatives...');
-%    diff = zeros(length(x),length(lambda));
-%    diff(:,1) = lambda(1) .* x.^(lambda(1)-1);
-%    for k = 2 : length(lambda)
-%       diff(:,k) = diff(:,k) + lambda(k).*val(:,k) + sum( val(:,1:k-1)*diag(lambda(1:k-1)+lambda(1:k-1)' + 1) , 2);  
-%       diff(:,k) = diff(:,k) ./ x;
-%    end
-% end
-%%
 if nargout > 1
-   disp('Caculating derivatives...');
+%   disp('Caculating derivatives...');
    diff = zeros(length(x),length(lambda));
    diff(:,1) = lambda(1) .* x.^(lambda(1)-1);
    for k = 2 : length(lambda)
       diff(:,k) = diff(:,k-1) + ( lambda(k) * val(:,k) + (1 + lambda(k-1))* val(:,k-1))./x;
    end
 end
-
-%%
-
